b797fda7ce4e48cbf8a8003bd1d221352ce0b899
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-04-25 02:41:54 +0300

    updated

--- src/pages/Accounts.vue
+++ src/pages/Accounts.vue
@@ -3,56 +3,32 @@
     <div class="account__wrapper">
       <header class="account__header">
         <div class="account__header__search">
-          <img
-            class="account__header__search-icon img -icon"
-            src="@/assets/img/icons/ic_search.svg"
-            alt=""
-          />
-          <input
-            class="account__header__search-input"
-            type="text"
-            placeholder="Search"
-          />
+          <img class="account__header__search-icon img -icon" src="@/assets/img/icons/ic_search.svg" alt="" />
+          <input class="account__header__search-input" type="text" placeholder="Search" />
         </div>
 
         <div class="account__header__info">
           <div class="account__header__info__notification">
-            <img
-              class="img -sm"
-              src="@/assets/img/icons/ic_notification.svg"
-              alt=""
-            />
+            <img class="img -sm" src="@/assets/img/icons/ic_notification.svg" alt="" />
             <div class="account__header__info__notification-badge"></div>
           </div>
 
-          <img
-            class="img -md"
-            src="@/assets/img/icons/ic_temp_avatar.svg"
-            alt=""
-          />
+          <img class="img -md" src="@/assets/img/icons/ic_temp_avatar.svg" alt="" />
         </div>
       </header>
       <p class="text -xxl -bold -center">Accounts</p>
       <div class="account__list">
-        <div
-          class="account__list__item"
-          v-for="(account, index) in accounts"
-          :key="index"
-        >
+        <div class="account__list__item" v-for="(account, index) in accounts" :key="index">
           <div class="account__list__item__info">
-            <img
-              class="img -lg"
-              :src="'/src/assets/img/icons/' + account.img + '.svg'"
-              alt=""
-            />
+            <img class="account__list__item__info-logo" :src="'/src/assets/img/logos/logo_' + account.img + '.png'" alt="" />
 
-            <div class="account__list__item__info-extra">
+            <div class="account__list__item__info-desc">
               <p class="text -xl -bold">{{ account.title }}</p>
-              <p class="text -md -medium">{{ account.site }}</p>
+              <p class="text -md -medium -dark-200">{{ account.site }}</p>
             </div>
           </div>
 
-          <p class="text -md -medium">{{ account.count }} Accounts</p>
+          <p class="text -md -medium">{{ account.count }} accounts</p>
 
           <Dropdown />
         </div>
@@ -76,22 +52,25 @@ export default {
           title: "Dropbox",
           site: "dropbox.com",
           count: 24,
-          img: "ic_temp_avatar",
+          img: "dropbox",
         },
+
         {
           id: 2,
-          title: "Google",
-          site: "google.com",
-          count: 24,
-          img: "ic_temp_avatar",
+          title: "Pinterest",
+          site: "pinterest.com",
+          count: 12,
+          img: "pinterest",
         },
+
         {
           id: 3,
-          title: "Facebook",
-          site: "facebook.com",
-          count: 24,
-          img: "ic_temp_avatar",
+          title: "Microsoft",
+          site: "microsft.com",
+          count: 3,
+          img: "microsoft",
         },
+
       ],
     };
   },
