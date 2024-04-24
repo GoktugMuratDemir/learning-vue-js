a1c9ef4580ff57befcffdc28b66c18328e6941ad
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-04-25 01:25:32 +0300

    dropdown added

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="dropdown">
+    <button class="dropdown__button">Dropdown</button>
+    <div class="dropdown__content">
+      <a href="#" v-for="(item, index) in dropdownItems" :key="index">{{
+        item
+      }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+
+  computed: {
+    dropdownItems() {
+      return ["Item 1", "Item 2", "Item 3"];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.dropdown {
+  display: inline-block;
+  position: relative;
+
+  &__button {
+    background-color: #4caf50;
+    border: none;
+    color: white;
+    cursor: pointer;
+    font-size: 16px;
+    padding: 16px;
+
+    &:hover,
+    &:focus {
+      background-color: #3e8e41;
+    }
+  }
+
+  &__content {
+    background-color: #f9f9f9;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    display: none;
+    min-width: 160px;
+    position: absolute;
+    z-index: 1;
+
+    a {
+      color: black;
+      display: block;
+      padding: 12px 16px;
+      text-decoration: none;
+
+      &:hover {
+        background-color: #f1f1f1;
+      }
+    }
+  }
+
+  &:hover {
+    .dropdown__content {
+      display: block;
+    }
+  }
+}
+</style>
--- src/pages/Accounts.vue
+++ src/pages/Accounts.vue
@@ -1,71 +1,99 @@
 <template>
-    <div class="account">
-        <div class="account__wrapper">
-            <header class="account__header">
-                <div class="account__header__search">
-                    <img class="account__header__search-icon img -icon" src="@/assets/img/icons/ic_search.svg" alt="" />
-                    <input class="account__header__search-input" type="text" placeholder="Search" />
-                </div>
-
-                <div class="account__header__info">
-                    <div class="account__header__info__notification">
-                        <img class="img -sm" src="@/assets/img/icons/ic_notification.svg" alt="" />
-                        <div class="account__header__info__notification-badge"></div>
-                    </div>
-
-                    <img class="img -md" src="@/assets/img/icons/ic_temp_avatar.svg" alt="" />
-                </div>
-            </header>
-            <p class="text -xxl -bold -center">Accounts</p>
-            <div class="account__list">
-                <div class="account__list__item" v-for="(account, index) in accounts" :key="index">
-                    <div class="account__list__item__info">
-                        <img class="img -lg" :src="'/src/assets/img/icons/' + account.img + '.svg'" alt="" />
-
-                        <div class="account__list__item__info-extra">
-                            <p class="text -xl -bold">{{ account.title }}</p>
-                            <p class="text -md -medium">{{ account.site }}</p>
-                        </div>
-                    </div>
-
-                    <p class="text -md -medium">{{ account.count }} Accounts</p>
-
-                    <p class="text -xxs">.....</p>
-                </div>
-            </div>
+  <div class="account">
+    <div class="account__wrapper">
+      <header class="account__header">
+        <div class="account__header__search">
+          <img
+            class="account__header__search-icon img -icon"
+            src="@/assets/img/icons/ic_search.svg"
+            alt=""
+          />
+          <input
+            class="account__header__search-input"
+            type="text"
+            placeholder="Search"
+          />
         </div>
+
+        <div class="account__header__info">
+          <div class="account__header__info__notification">
+            <img
+              class="img -sm"
+              src="@/assets/img/icons/ic_notification.svg"
+              alt=""
+            />
+            <div class="account__header__info__notification-badge"></div>
+          </div>
+
+          <img
+            class="img -md"
+            src="@/assets/img/icons/ic_temp_avatar.svg"
+            alt=""
+          />
+        </div>
+      </header>
+      <p class="text -xxl -bold -center">Accounts</p>
+      <div class="account__list">
+        <div
+          class="account__list__item"
+          v-for="(account, index) in accounts"
+          :key="index"
+        >
+          <div class="account__list__item__info">
+            <img
+              class="img -lg"
+              :src="'/src/assets/img/icons/' + account.img + '.svg'"
+              alt=""
+            />
+
+            <div class="account__list__item__info-extra">
+              <p class="text -xl -bold">{{ account.title }}</p>
+              <p class="text -md -medium">{{ account.site }}</p>
+            </div>
+          </div>
+
+          <p class="text -md -medium">{{ account.count }} Accounts</p>
+
+          <Dropdown />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Dropdown from "../components/Dropdown.vue";
+
 export default {
-    components: {},
-    data() {
-        return {
-            accounts: [
-                {
-                    id: 1,
-                    title: "Dropbox",
-                    site: "dropbox.com",
-                    count: 24,
-                    img: "ic_temp_avatar",
-                },
-                {
-                    id: 2,
-                    title: "Google",
-                    site: "google.com",
-                    count: 24,
-                    img: "ic_temp_avatar",
-                },
-                {
-                    id: 3,
-                    title: "Facebook",
-                    site: "facebook.com",
-                    count: 24,
-                    img: "ic_temp_avatar",
-                },
-            ],
-        };
-    },
+  components: {
+    Dropdown
+  },
+  data() {
+    return {
+      accounts: [
+        {
+          id: 1,
+          title: "Dropbox",
+          site: "dropbox.com",
+          count: 24,
+          img: "ic_temp_avatar",
+        },
+        {
+          id: 2,
+          title: "Google",
+          site: "google.com",
+          count: 24,
+          img: "ic_temp_avatar",
+        },
+        {
+          id: 3,
+          title: "Facebook",
+          site: "facebook.com",
+          count: 24,
+          img: "ic_temp_avatar",
+        },
+      ],
+    };
+  },
 };
 </script>
