69fa13dfc3f9df8a2e9c57d5ead2ddcaef0a3203
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-04-24 15:17:42 +0300

    updated

--- src/components/checkboxs/AuthCheckBox.vue
+++ src/components/checkboxs/AuthCheckBox.vue
@@ -1,28 +1,26 @@
 <template>
-    <div class="checkbox">
-        <label class="checkbox__label">
-            <input class="checkbox__input" type="checkbox" name="" id="" />{{ label }}
-        </label>
-        <router-link :to="linkHref" class="text -link">{{ linkText }}</router-link>
-    </div>
+  <div class="checkbox">
+    <label class="checkbox__label">
+      <input class="checkbox__input" type="checkbox" name="" id="" />{{ label }}
+    </label>
+    <router-link :to="linkHref" class="text -link">{{ linkText }}</router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AuthCheckBox',
-    props: {
-        label: {
-            type: String,
-
-        },
-        linkText: {
-            type: String,
-
-        },
-        linkHref: {
-            type: String,
-            default: '/'
-        }
-    }
-}
+  name: "AuthCheckBox",
+  props: {
+    label: {
+      type: String,
+    },
+    linkText: {
+      type: String,
+    },
+    linkHref: {
+      type: String,
+      default: "/",
+    },
+  },
+};
 </script>
--- src/components/propmts/AuthPrompt.vue
+++ src/components/propmts/AuthPrompt.vue
@@ -1,27 +1,30 @@
 <template>
-    <div>
-        <p class="text -xs -medium -dark-400">
-            {{ promptText }}  <router-link :to="linkHref" class="text -link">{{ linkText }}</router-link>
-        </p>
-    </div>
+  <div>
+    <p class="text -xs -medium -dark-400">
+      {{ promptText }}
+      <router-link :to="linkHref" class="text -link">{{
+        linkText
+      }}</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AuthPrompt',
-    props: {
-        promptText: {
-            type: String,
-            required: true
-        },
-        linkText: {
-            type: String,
-            required: true
-        },
-        linkHref: {
-            type: String,
-            default: '/'
-        }
-    }
-}
+  name: "AuthPrompt",
+  props: {
+    promptText: {
+      type: String,
+      required: true,
+    },
+    linkText: {
+      type: String,
+      required: true,
+    },
+    linkHref: {
+      type: String,
+      default: "/",
+    },
+  },
+};
 </script>
