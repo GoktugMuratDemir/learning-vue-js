63044eaeab6a60f21e9076db5c9c19a2aa244db0
GoktugMuratDemir <goktugdemirwebdev@gmail.com>
2024-04-25 14:29:58 +0300

    updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import ProductCardInfoUiDesign from "../pages/ProductCardInfoUiDesign.vue";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
-import Accounts from "../pages/Accounts.vue";
+import AccountList from "../pages/AccountList.vue";
+import ActiviteList from "../pages/ActiviteList.vue";
 
 export const routes = [
   {
@@ -17,16 +18,20 @@ export const routes = [
     component: Register,
   },
   {
+    path: "/activite",
+    name: "Activite List",
+    component: ActiviteList,
+  },
+  {
     path: "/accounts",
-    name: "Accounts",
-    component: Accounts,
+    name: "Account List",
+    component: AccountList,
   },
   {
     path: "/product-card-info-ui-design",
-    name: "Product Card Info UI Design",
+    name: "Product Card",
     component: ProductCardInfoUiDesign,
-  }
-
+  },
 ];
 
 const router = createRouter({
